b8626dda373e30431d856d0689d597dca2a7fd69
fxss5201 <fxss5201@users.noreply.github.com>
2023-01-29 10:26:24 +0000

    deploy: 15640cbc927b67808ac48de3bd99fd6acf90f115

---
+++ static/js/793.77811a47.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_antd=self.webpackChunkreact_antd||[]).push([[793],{73006:function(e,n,t){t.r(n);var a=t(80184);n.default=function(){return(0,a.jsx)(a.Fragment,{children:"PageAdmin: \u7ba1\u7406\u5458\u6743\u9650\u7528\u6237\u53ef\u4ee5\u770b\u5230\u8be5\u9875\u9762"})}}}]);
+//# sourceMappingURL=793.77811a47.chunk.js.map
